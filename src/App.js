358fdd7b3df7759aa17a18b714161fd76a96b178
Allen Wang <allen.y.w@berkeley.edu>
2017-03-29 22:48:01 -0700

    Troll flappy birds

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import Slider from 'material-ui/Slider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Tone from 'tone';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 100,
+  },
+
+};
+
+function handleActive(tab) {
+  alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
+
+}
+
 
 class App extends Component {
+  componentDidMount() {
+    
+  }
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider>
+      <div className="App" style={{}}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to CSCS</h2>
+          <h2>Welcome to CSCS2</h2>
         </div>
-        <p className="App-intro">
-          In Beta
-        </p>
+      <Tabs>
+          <Tab label="Item One" >
+            <div>
+              <h2 style={styles.headline}>Tab One</h2>
+              <p>
+                This is an example tab.
+              </p>
+              <p>
+                You can put any sort of HTML or react component in here. It even keeps the component state!
+              </p>
+              <Slider name="slider0" defaultValue={0.5} />
+            </div>
+          </Tab>
+          <Tab label="Item Two" >
+
+          </Tab>
+          <Tab
+            label="onActive"
+            data-route="/home"
+            onActive={handleActive}
+          >
+            <div>
+              <h2 style={styles.headline}>Tab Three</h2>
+              <p>
+                This is a third example tab.
+              </p>
+            </div>
+          </Tab>
+        </Tabs>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
